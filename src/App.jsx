cfd00a94aadc8659cc862278694fff595b838762
lannory <detochkanikolay@gmail.com>
2025-06-03 18:25:07 +0300

    hw was competed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,3 @@
-
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { useDispatch, useSelector } from 'react-redux'
 import { decrease, increase } from './store/counterSlice'
